6850d5aada32da889a2c6f4c355c6e373e3c7193
Pablo Cardozo <pablocyc83@gmail.com>
2020-04-21 19:35:38 -0400

    Metodos II

---
+++ course.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Course struct {
+  Name    string
+  Price   float64
+  IsFree  bool
+  UserIDs []uint
+  Classes map[uint]string
+}
+
+func (c Course) PrintClasses() {
+  text := "Las clases son: "
+  for _, class := range c.Classes {
+    text += class + ", "
+  }
+
+  fmt.Println(text[:len(text) - 2])
+}
+
+
--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import "fmt"
-
-type Course struct {
-  Name    string
-  Price   float64
-  IsFree  bool
-  UserIDs []uint
-  Classes map[uint]string
-}
-
-func (c Course) PrintClasses() {
-  text := "Las clases son: "
-  for _, class := range c.Classes {
-    text += class + ", "
-  }
-
-  fmt.Println(text[:len(text) - 2])
-}
-
 func main() {
   Go := Course {
     "Go desde cero",
